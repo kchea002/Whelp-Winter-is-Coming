00ecb76384a3665003409c505bb0089348b674c2
Kevin Cheang <cheangk009@gmail.com>
2019-05-02 18:34:53 -0700

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create(email: "jonsnow@westeros.com", password: "password")
+demo = User.create({email: "jonsnow@westeros.com", password: "password"})
