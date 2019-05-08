d1825a76a3efc34255972f8fa09c04f65211b1f9
Kevin Cheang <cheangk009@gmail.com>
2019-05-08 12:09:43 -0700

    remove config

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-config.assets.initialize_on_precompile = false
+# config.assets.initialize_on_precompile = false
 
 require 'rails/all'
 
--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -3,4 +3,29 @@ import * as APIutil from '../util/review_api_util';
 export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
 export const REMOVE_REVIEW = 'REMOVE_REVIEW';
 
-export const receiveReview = ({ review, average_rating, author }) =>
+export const receiveReview = ({ review, average_rating, author }) => ({
+    type: RECEIVE_REVIEW,
+    review,
+    average_rating,
+    author
+});
+
+export const removeReview = payload => ({
+    type: REMOVE_REVIEW,
+    review: payload.review
+});
+
+export const createReview = review => dispatch => (
+    APIutil.createReview(review)
+        .then(review => dispatch(receiveReview(review)))
+);
+
+export const updateReview = review => dispatch => (
+    APIutil.updateReview(review)
+        .then(review => dispatch(receiveReview(review)))
+);
+
+export const deleteReview = id => dispatch => (
+    APIutil.deleteReview(id)
+        .then(payload => dispatch(removeReview(payload)))
+);
