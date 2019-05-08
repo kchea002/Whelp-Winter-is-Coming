dea340bacb26d2041a11288cccab136bbc7d9e45
Kevin Cheang <cheangk009@gmail.com>
2019-05-08 14:54:57 -0700

    removed Ryan's comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'open_uri'
+# require 'open_uri'
 
 User.destroy_all
 Business.destroy_all
