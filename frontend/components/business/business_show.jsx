da65b76c973a0c46b753474f7d306625f38cc319
Kevin Cheang <cheangk009@gmail.com>
2019-07-14 11:47:40 -0700

    fix pictures collapsing in the show page on resize(smaller)

--- frontend/components/business/business_show.jsx
+++ frontend/components/business/business_show.jsx
@@ -24,27 +24,21 @@ class BusinessShow extends React.Component {
 
     render() {
         
-       
-        
-        let business = this.props.business
-        
+        let business = this.props.business;
         if (!business) return null;
-        
-
         let buttonLink = `/biz/${this.props.businessId}/review`;
 
 
-        let businesspictures = null;
+        let businessPictures = null;
         if (business.pictures) {
-            businesspictures = <>  
+            businessPictures = <>  
                 <img className="show-image-container" src={business.pictures[0]} />
                 <img className="show-image-container" src={business.pictures[1]} /> 
             </>
         } 
 
-    
+        let averageRating = Math.floor(business.average_rating * 2)
         const reviews = this.props.reviews.map(review => {
-        
             return (
                 <ReviewsContainer key={review.id} review={review} />
             )
@@ -67,19 +61,21 @@ class BusinessShow extends React.Component {
                             <GreetingContainer />
                         </div>
                     </div>
-                  
-
-
                 </div>
                 <div className="index-tab-bar"></div>
-                <div className="show-title">
-                    <div className="show-title-body">
-                        <div className="show-title-name">{business.name}</div>
-                        <div> CROWN PLACEHOLDER: 8 reviews</div>
-                        <div>{business.price}</div>
-                    </div>
-                    <div className="review-background">
-                        <Link to={buttonLink}> <button id="write-button">Write A Review</button></Link>
+                <div className="show-title-center">
+                    <div className="show-title">
+                        <div className="show-title-body">
+                            <div className="show-title-name">{business.name}</div>
+                            <div className="show-stars-review-count">
+                                <img className={`stars-large-${averageRating}` + ' stars-large'} src="https://i.imgur.com/UkZkm0D.png" />
+                                <p> {this.props.reviews.length} reviews </p>
+                            </div>
+                            <div className="show-price" >{business.price}</div>
+                        </div>
+                        <div className="review-background">
+                            <Link to={buttonLink}> <button id="write-button">Write A Review</button></Link>
+                        </div>
                     </div>
                 </div>
                 <div className="show-pictures">
@@ -90,9 +86,9 @@ class BusinessShow extends React.Component {
                             <div>{business.location}</div>
                         </div>
                     </div>
-                    <div >
+                    <div className="show-image-whole-container">
                         <img className="show-image-container" src={business.photo} />
-                        {businesspictures}
+                        {businessPictures}
                     </div>
                 </div>
 
