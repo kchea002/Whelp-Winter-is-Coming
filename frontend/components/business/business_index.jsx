de9dd11f3700cb842105403776a96a4c6fd4c589
Kevin Cheang <cheangk009@gmail.com>
2019-07-21 14:20:49 -0700

    add media queries for responsiveness

--- frontend/components/business/business_index.jsx
+++ frontend/components/business/business_index.jsx
@@ -12,14 +12,14 @@ class BusinessIndex extends React.Component {
         this.searchLocation = this.props.searchLocation;
     }
 
-    render(){
-    
-        const { businesses, history } = this.props; 
+    render() {
+
+        const { businesses, history } = this.props;
         let allBusinesses = businesses.map((business, idx) => {
             return (
-                <BusinessItemContainer 
-                    key={business.id} 
-                    business={business} 
+                <BusinessItemContainer
+                    key={business.id}
+                    business={business}
                 />
             )
         });
@@ -29,11 +29,11 @@ class BusinessIndex extends React.Component {
                 <div className="login-header-index">
                     <div className="login-header-mid">
                         <Link to={'/'}>
-                            <img className="login-logo" 
+                            <img className="login-logo"
                                 src="https://i.imgur.com/O22tB8P.png" />
                         </Link>
-                        <Minisearch 
-                            searchLocation={this.searchLocation} 
+                        <Minisearch
+                            searchLocation={this.searchLocation}
                             history={history} />
                     </div>
                     <div className="right-nav-three">
@@ -41,26 +41,26 @@ class BusinessIndex extends React.Component {
                     </div>
                 </div>
                 <div className="index-tab-bar"></div>
-                <div className="index-title-bar"> 
+                <div className="index-title-bar">
                     <div>
-                        <p> Browsing businesses in Westeros</p> 
+                        <p> Browsing businesses in Westeros</p>
                     </div>
                 </div>
                 <div className="index-item-container" >
                     <ul>
                         {allBusinesses}
-                    </ul> 
+                    </ul>
                     <img id="map" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/41cbc76b-d7cc-48de-aa20-38a58b895935/dal295u-e59f3044-9052-4c4d-a529-23b140982c70.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQxY2JjNzZiLWQ3Y2MtNDhkZS1hYTIwLTM4YTU4Yjg5NTkzNVwvZGFsMjk1dS1lNTlmMzA0NC05MDUyLTRjNGQtYTUyOS0yM2IxNDA5ODJjNzAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.U6gYZ37gHnrL75Gl00G_SsACGNh3tp5QkOl9VqGHXwk" />
                 </div>
                 <div>
-                    
+
                 </div>
-              
+
             </div>
 
-        )     
+        )
     }
 }
-    
+
 
 export default BusinessIndex;
